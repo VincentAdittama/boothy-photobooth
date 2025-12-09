603aa3f2e5e2a81ac7c7a1c7121a88c9a1c3b00e
Vincent <eezaateedanvers@gmail.com>
2025-12-09 22:47:20 +0700

    feat: Add basic SSL plugin for Vite configuration and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
+  server: {
+    https: true,
+    host: true,
+  },
 })
